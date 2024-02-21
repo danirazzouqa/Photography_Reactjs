3563062f66917ac54e7868e64343d0374a0d3a74
dany <danirazzouqa@gmail.com>
2024-02-21 02:21:49 +0100

    Finished Projectp

--- FrontEnd/src/hooks/useSignup.jsx
+++ FrontEnd/src/hooks/useSignup.jsx
@@ -11,7 +11,7 @@ setIsLoading(true)
 setError(null)
 
 
-const response = await fetch('http://localhost:4000/users/signup', {
+const response = await fetch('https://photography-reactjs.onrender.com/users/signup', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({username ,email, password})
